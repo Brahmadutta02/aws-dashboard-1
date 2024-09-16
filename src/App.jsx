4e90875decd5b5489b4e7dcb7a9c1f278c163dc5
brahmadutta02 <brahmaduttadalai012@gmail.com>
2024-09-17 00:27:11 +0530

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import {
 // import './App.css'
 import { Amplify } from 'aws-amplify';
 import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+// import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 
 Amplify.configure(awsExports);
